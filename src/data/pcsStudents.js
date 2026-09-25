66600c16067aa6382932e4cf5c246f08cbbf764e
Rohitbhai9765 <41791781+Rohitbhai9765@users.noreply.github.com>
2026-09-25 14:16:19 +0530

    Update pcsStudents.js

--- src/data/pcsStudents.js
+++ src/data/pcsStudents.js
@@ -1,18 +1,15 @@
 export const pcsStudents = [
   { srNo: 1, mis: "612301003", name: "ANJALI JADHAV" },
   { srNo: 2, mis: "612301021", name: "CHITTORA RITESH PURUSHOTTAM" },
-  { srNo: 3, mis: "612301029", name: "DUDE CHUZHO" },
-  { srNo: 4, mis: "612301038", name: "KADAM ANISH ANIL" },
-  { srNo: 5, mis: "612301043", name: "KEDAR RITESH RHUSHIKESH" },
-  { srNo: 30, mis: "612301051", name: "MANSI SAMBHAJI CHOPADE" },
+  { srNo: 3, mis: "612301038", name: "KADAM ANISH ANIL" },
+  { srNo: 4, mis: "612301043", name: "KEDAR RITESH RHUSHIKESH" },
+  { srNo: 5, mis: "612301051", name: "MANSI SAMBHAJI CHOPADE" },
   { srNo: 6, mis: "612301053", name: "MOHITE AAKANKSHA VISHWAS" },
-  { srNo: 7, mis: "612301058", name: "MUSTAFA ALI" },
-  { srNo: 8, mis: "612301071", name: "PATIL RASHIKA PRASHANT" },
-  { srNo: 9, mis: "612301077", name: "PULOHITO Y ACHUMI" },
-  { srNo: 10, mis: "612301079", name: "RATHI PRANJAL SANTOSH" },
-  { srNo: 11, mis: "612301082", name: "ROHIT BHAGAT" },
-  { srNo: 12, mis: "612301084", name: "SHELAR SHREYA RAJU" },
-  { srNo: 13, mis: "612301086", name: "SOMANI DEVEN SHRIKANT" },
-  { srNo: 14, mis: "612301089", name: "TAGADE SAMRUDDHI MAHESH" },
-  { srNo: 15, mis: "612301091", name: "THOMBARE RISHI DINESH" }
+  { srNo: 7, mis: "612301071", name: "PATIL RASHIKA PRASHANT" },
+  { srNo: 8, mis: "612301079", name: "RATHI PRANJAL SANTOSH" },
+  { srNo: 9, mis: "612301082", name: "ROHIT BHAGAT" },
+  { srNo: 10, mis: "612301084", name: "SHELAR SHREYA RAJU" },
+  { srNo: 11, mis: "612301086", name: "SOMANI DEVEN SHRIKANT" },
+  { srNo: 12, mis: "612301089", name: "TAGADE SAMRUDDHI MAHESH" },
+  { srNo: 13, mis: "612301091", name: "THOMBARE RISHI DINESH" }
 ];
